ea937eff60b53547e516afd187d0c09d97d22ecc
bahman <bahmansalehinic@gmail.com>
2022-07-10 14:39:27 -0400

    adds functions

---
+++ src/domain/planets.py
@@ -0,0 +1,68 @@
+import requests
+import unicodedata
+
+SOLAR_API_URL = 'https://api.le-systeme-solaire.net/rest/bodies'
+
+
+def get_data_from_external_api(api_url):
+    response = requests.get(api_url)
+    return response.json()
+
+
+def strip_accents(text):
+    """ for converting Latin letters to English i.e: é --> e"""
+    return ''.join(char for char in
+                   unicodedata.normalize('NFKD', text)
+                   if unicodedata.category(char) != 'Mn')
+
+
+class PlanetsDataCollector:
+    def __init__(self):
+        self.final_data = None
+        self.planets_raw_data = None
+        self.solar_data = get_data_from_external_api(SOLAR_API_URL)
+        self.filter_all_planet_data()
+
+    def filter_all_planet_data(self):
+        all_planet_data = [item for item in self.solar_data['bodies'] if item.get("isPlanet")]
+        self.planets_raw_data = all_planet_data
+
+    @staticmethod
+    def planet_specific_moons(planet_specific_data):
+        moons = planet_specific_data.get('moons', None)
+        return moons
+
+    def filter_moon_data(self,moon_id):
+        moon_data = [item for item in self.solar_data['bodies'] if item.get("id")
+                     in [moon_id, strip_accents(moon_id.lower())]]
+        return moon_data
+
+    def extract_planet_moons_data(self, planet_specific_data):
+        planet_moon_data = self.planet_specific_moons(planet_specific_data)
+        detailed_data = [self.filter_moon_data(item['moon']) for item in planet_moon_data]
+        return detailed_data
+
+    def sort_planet_moons_data_by_mass(self, planet_moon_data):
+        pass
+
+    def find_planet_smallest_moon(self, planet_sorted_moon_data):
+        pass
+
+    def pre_process_planet_data(self, ):
+        for p in self.planets_raw_data:
+            pass
+
+
+
+if __name__ == '__main__':
+    url = SOLAR_API_URL
+    # #params = {'filter[]':'isPlanet,neq,false'}
+    # data = get_data_from_external_api(url)#, params)
+    p_data = PlanetsDataCollector()
+    planet_data = p_data.planets_raw_data
+    uranus_data = planet_data[0]
+    print(uranus_data['moons'])
+    uranus_moons_data = p_data.planet_specific_moons(uranus_data)
+    # print(uranus_moons_data)
+    # print(filter_moon_data(data, 'Ariel'))
+    print(p_data.extract_planet_moons_data(uranus_data))
